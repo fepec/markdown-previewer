ea0a32690b167813701bb299d50bb82abbc51f16
fepec <felipe.estrada.prada@gmail.com>
2023-11-14 17:32:45 -0500

    Add state handlers and props to children

--- src/App.js
+++ src/App.js
@@ -1,50 +1,9 @@
 import EditorWindow from "./components/EditorWindow"
 import PreviewWindow from "./components/PreviewWindow"
 
-const defaultText = `# Welcome to my React Markdown Previewer!
-
-## This is a sub-heading...
-### And here's some other cool stuff:
-
-Heres some code, \`<div></div>\`, between 2 backticks.
-
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
-
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.org), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | -------------
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
 
 
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. And last but not least, let's not forget embedded images:
-
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
-`
+const defaultText = 'Hello Zuri'
 
 export default class App extends React.Component {
     
@@ -53,18 +12,24 @@ export default class App extends React.Component {
         this.state = {
           text: defaultText
         };
-    }
+        this.handleChange = this.handleChange.bind(this)
+    };
 
     // State
-
+    
     // handlers
+    handleChange(e) {
+        this.setState({
+            text: e.target.value
+        })
+    }
 
     // renders
     render() {
     return (
         <>
-        <EditorWindow />
-        <PreviewWindow />
+        <EditorWindow handleChange={this.handleChange} text={this.state.text} />
+        <PreviewWindow text={this.state.text}/>
         </>
     );
     }
