26d6f35c9372375bccd052e519a60dae463917bd
fepec <felipe.estrada.prada@gmail.com>
2023-11-14 15:54:40 -0500

    Change App to handle state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import EditorWindow from "./components/EditorWindow"
 import PreviewWindow from "./components/PreviewWindow"
 
-export default function App() {
+export default class App extends React.Component {
 
+    render() {
     return (
         <>
         <EditorWindow />
         <PreviewWindow />
         </>
     );
+    }
 }
